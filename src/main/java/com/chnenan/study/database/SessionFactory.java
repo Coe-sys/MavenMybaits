2499e907ef8355006d60932e011b4d914d14418a
Corrol <Chen_MuNan@163.com>
2019-11-27 15:32:20 +0800

    test

---
+++ src/main/java/com/chnenan/study/database/SessionFactory.java
@@ -0,0 +1,35 @@
+package com.chnenan.study.database;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * demo class
+ * 每个基于 MyBatis 的应用都是以一个 SqlSessionFactory 的实例为核心的。SqlSessionFactory 的实例可以通过 SqlSessionFactoryBuilder 获得。
+ * 而 SqlSessionFactoryBuilder 则可以从 XML 配置文件或一个预先定制的 Configuration 的实例构建出 SqlSessionFactory 的实例。
+ * @author 陈楠
+ * @date 2019/11/27 14:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class SessionFactory {
+    static String resource = "mybatis-config.xml";
+    public static SqlSessionFactory sessionFactory;
+
+    static {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSession(){
+        return sessionFactory.openSession();
+    }
+}
---
+++ src/main/java/com/chnenan/study/database/data/TDemoFile.java
@@ -0,0 +1,98 @@
+package com.chnenan.study.database.data;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * demo class
+ * 文件表的持久化类
+ * @author 陈楠
+ * @date 2019/11/27 11:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class TDemoFile implements Serializable {
+    /**
+     * 文件ID
+     */
+    private Integer fileId;
+    /**
+     * 文件名（逻辑名称：展示给用户看的名称）
+     */
+    private String fileName;
+    /**
+     * 文件路径（物理名称--磁盘中的名称）
+     */
+    private String fileUrl;
+    /**
+     * 文件类型
+     */
+    private String fileType;
+    /**
+     * 文件长度
+     */
+    private Integer fileSize;
+    /**
+     * 操作时间
+     */
+    private Timestamp operTime;
+
+    public TDemoFile() {
+    }
+
+    public TDemoFile(String fileName, String fileUrl, String fileType,
+                     Integer fileSize, Timestamp operTime) {
+        this.fileName = fileName;
+        this.fileUrl = fileUrl;
+        this.fileType = fileType;
+        this.fileSize = fileSize;
+        this.operTime = operTime;
+    }
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Integer fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Timestamp getOperTime() {
+        return operTime;
+    }
+
+    public void setOperTime(Timestamp operTime) {
+        this.operTime = operTime;
+    }
+}
